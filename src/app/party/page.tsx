41943ad5d599b2b60f22c441f64d813bb86d02b6
Mateus Vallim <mateus.vallim@usp.br>
2023-08-27 18:53:25 -0300

    configurate Prettier Eslint and Tailwind-Prettier

---
+++ src/app/party/page.tsx
@@ -0,0 +1,8 @@
+export default function Party() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Festas</h1>
+      </main>
+    )
+  }
+  
