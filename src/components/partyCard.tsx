71ccc664d144e0d96f727e5600bce3c790c0f324
Mateus Vallim <mateus.vallim@usp.br>
2023-08-28 06:09:52 -0300

    fixed link images

--- src/components/partyCard.tsx
+++ src/components/partyCard.tsx
@@ -13,7 +13,7 @@ export default function PartyCard({date, cover, name, fid, isNew}: PartyCardProp
     return (
         <Link href={!isNew ? `/party/${fid}` : ""} className="w-80 border-2 border-zinc-900 rounded-lg overflow-hidden flex flex-col hover:shadow-zinc-950 hover:shadow-lg hover:brightness-90 transition-all">
             <Image
-                src={`https://drive.google.com/uc?id=${cover}&export=download`}
+                src={`https://drive.google.com/uc?id=${cover}`}
                 alt={`Cover image for party ${name}.`}
                 width={320}
                 height={240}
