e914755dde4a156ed80a9a3c5dfdba1646e0c77b
Mateus Vallim <mateus.vallim@usp.br>
2023-09-01 16:47:14 -0300

    try to compile again

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
-    measurementId: process.env.MEASUREMENTID
-  };
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
+}
 
 const app = initializeApp(firebaseConfig)
 
