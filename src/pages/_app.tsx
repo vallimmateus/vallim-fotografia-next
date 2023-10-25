f02b7b482edf6096d32c47dbf313e2cb232cbc81
Mateus Vallim <mateus.vallim@usp.br>
2023-10-25 11:36:04 -0300

    chore: add init migration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from 'next'
-import type { AppProps } from 'next/app'
+import type { Metadata } from "next";
+import type { AppProps } from "next/app";
 
-import './globals.css'
-import { GlobalProps } from '@/features/GlobalProps/GlobalProps'
-import Layout from '@/components/Layout'
-import GoogleProviders from '@/components/GoogleProviders'
-import { GlobalPropsContextProvider } from '@/features/GlobalProps/contexts/GlobalPropsContext'
+import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import { GlobalProps } from "@/features/GlobalProps/GlobalProps";
+import Layout from "@/components/Layout";
+import { GlobalPropsContextProvider } from "@/features/GlobalProps/contexts/GlobalPropsContext";
 
 export const metadata: Metadata = {
-  title: 'Vallim Fotografia',
-  description: 'Fotógrafo da universidade EEL-USP',
-}
+  title: "Vallim Fotografia",
+  description: "Fotógrafo da universidade EEL-USP",
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalPropsContextProvider globalProps={GlobalProps.extract(pageProps)}>
       <div className="bg-zinc-900 text-white">
-        <GoogleProviders>
+        <SessionProvider session={pageProps.session}>
           <Layout {...pageProps}>
             <Component {...pageProps} />
           </Layout>
-        </GoogleProviders>
+        </SessionProvider>
       </div>
     </GlobalPropsContextProvider>
-  )
+  );
 }
