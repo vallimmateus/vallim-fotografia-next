d7c0b6727cd59c5163ce2d1f592b0bbfa8b21669
Mateus Vallim <mateus.vallim@usp.br>
2024-02-27 12:09:46 -0300

    fix: fix the getFullYear function on header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -172,9 +172,9 @@ export function Header() {
                           <NavigationMenuLink asChild>
                             <Link
                               className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                              href={`/event/${party.date.getFullYear()}/${
-                                party.slug
-                              }`}
+                              href={`/event/${new Date(
+                                party.date,
+                              ).getFullYear()}/${party.slug}`}
                             >
                               <div className="text-sm font-medium leading-none">
                                 {party.name}
