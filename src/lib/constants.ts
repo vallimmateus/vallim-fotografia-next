9dd60ac770da14c83531fcd2a43412ace7cc37d8
Mateus Vallim <mateus.vallim@usp.br>
2024-09-12 01:02:47 -0300

    refactor: Update basePath logic in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,8 @@
 export const basePath =
-  process.env.NEXT_PUBLIC_VERCEL_URL ||
-  process.env.NEXT_PUBLIC_BASE_PATH ||
+  (process.env.NEXT_PUBLIC_VERCEL_URL &&
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`) ||
+  (process.env.NEXT_PUBLIC_BASE_PATH &&
+    (!process.env.NEXT_PUBLIC_BASE_PATH.includes('https://')
+      ? `https://${process.env.NEXT_PUBLIC_BASE_PATH}`
+      : process.env.NEXT_PUBLIC_BASE_PATH)) ||
   'http://localhost:3000'
