f1a096d99c5ffeb3a80eed1cef70ed7af0a405f8
Mateus Vallim <mateus.vallim@usp.br>
2025-05-06 21:10:44 -0300

    refactor: remover rotas de API não utilizadas para simplificar a estrutura

--- src/app/api/organizations/[organizationId]/members/route.ts
+++
@@ -1,2 +0,0 @@
-// POST /api/organizations/[organizationId]/members (registrar membros de uma organização)
-// DELETE /api/organizations/[organizationId]/members (deletar membros de uma organização)
--- src/app/api/organizations/[organizationId]/route.ts
+++
@@ -1,3 +0,0 @@
-// GET /api/organizations/[organizationId] (detalhar uma organização)
-// UPDATE /api/organizations/[organizationId] (atualizar uma organização)
-// DELETE /api/organizations/[organizationId] (deletar uma organização)
