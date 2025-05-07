e0f75d84bc8559d3811d04cd531fddb082e90fd0
Mateus Vallim <mateus.vallim@usp.br>
2025-05-06 23:17:25 -0300

    refactor: melhorar a gestão do cliente Prisma e adicionar tratamento de erros na função getOrganizations

--- src/app/event/register/actions/get-organizations.ts
+++ src/app/event/register/actions/get-organizations.ts
@@ -4,17 +4,22 @@ import { prismaClient } from '@/lib/prisma'
 import { Organization } from '@prisma/client'
 
 export async function getOrganizations() {
-  const organizations = await prismaClient.organization.findMany({
-    orderBy: {
-      name: 'asc',
-    },
-  })
+  try {
+    const organizations = await prismaClient.organization.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    })
 
-  const organizationsWithLogo: Array<Organization & { logoUrl: string }> =
-    organizations.map((organization) => ({
-      ...organization,
-      logoUrl: `/images/${organization.logoS3Key}`,
-    }))
+    const organizationsWithLogo: Array<Organization & { logoUrl: string }> =
+      organizations.map((organization) => ({
+        ...organization,
+        logoUrl: `/images/${organization.logoS3Key}`,
+      }))
 
-  return organizationsWithLogo
+    return organizationsWithLogo
+  } catch (error) {
+    console.error('Error fetching organizations:', error)
+    throw new Error('Failed to fetch organizations')
+  }
 }
