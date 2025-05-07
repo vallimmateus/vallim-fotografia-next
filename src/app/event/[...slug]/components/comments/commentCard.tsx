cda46c91fdfbc8030abe5023b20728f54e07efb8
Mateus Vallim <mateus.vallim@usp.br>
2025-05-06 21:46:12 -0300

    refactor: ajustar verificação de função de usuário e melhorar acessibilidade do botão de comentários

--- src/app/event/[...slug]/components/comments/commentCard.tsx
+++ src/app/event/[...slug]/components/comments/commentCard.tsx
@@ -9,7 +9,6 @@ import { CommentWithUserType } from './commentsContext'
 
 import { User } from '@prisma/client'
 import { Pencil2Icon, PersonIcon, TrashIcon } from '@radix-ui/react-icons'
-import axios from 'axios'
 import { format } from 'date-fns'
 import { deleteComment, updateComment } from './actions/comments'
 
@@ -38,7 +37,7 @@ export default function CommentCard({
 
   useEffect(() => {
     getUser().then((user) => {
-      if (user.role === 'admin') {
+      if (user.roles.includes('admin')) {
         setDeletable(true)
       } else {
         setEditable(user.email === email)
@@ -78,7 +77,7 @@ export default function CommentCard({
   return (
     <div className="m-2 flex gap-3 rounded-md bg-zinc-700 p-2">
       <Avatar className="h-6 w-6">
-        <AvatarImage src={image} alt={nickname || name} />
+        <AvatarImage src={image || ''} alt={nickname || name || ''} />
         <AvatarFallback>
           <PersonIcon className="h-5 w-5" />
         </AvatarFallback>
